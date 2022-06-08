225f615b569f8aa01563ea76fafbfa1f14d9f152
Suraj Chakravarthi <cluster.root@gmail.com>
2022-06-08 15:39:14 -0700

    Made a simple placeholder wxWidgets app.

--- include/telecon.h
+++ include/telecon.h
@@ -3,6 +3,33 @@
 #ifndef TELECON_H
 #define TELECON_H
 
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif // !WX_PRECOMP
+
+#define APP_NAME "telecon"
+#define ICON_LOC "../resources/telecon_icon.png"
+
+/**
+ * telecomApp is the foundational wxWidgets class of telecon, derived from wxApp.
+ */
+class teleconApp : public wxApp
+{
+public:
+	/** Overridden Oninit function is where user program functionalities begin.*/
+	virtual bool OnInit();
+};
+
+/**
+ * This class defines the object of the primary window of telecon.
+ */
+class mainFrame : public wxFrame
+{
+public:
+	mainFrame(const wxString& title);
+};
 
 #endif // !TELECON_H
 

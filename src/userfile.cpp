7c92e8a5eb0c390a5f72e99923c58ffca6928764
Darren Huang <darrenhu@usc.edu>
2023-04-30 01:40:28 -0700

    main in userfile commented

--- src/userfile.cpp
+++ src/userfile.cpp
@@ -14,6 +14,9 @@
 
 #include "wxchartviewer.h"
 
+
+// Helper functions to generate random data points
+
 double generateRandomDouble(double min, double max)
 {
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
@@ -38,88 +41,106 @@ void printHello(){
     
 }
 
+
+
+/**
+ * Here is an example of how to use Telecon.
+ */
 int main(int argc, char* argv[])
 {
+    // Constants
     float test = .1;
     cout << std::to_string(test) << endl;
     const int dataRateMillis = 100;
-    // Initialization code
+
+
+    // START SETTING UP COMPONENTS TO BE USED FOR TELEMETRY AND CONTROLS
+
+
+    // Initialization code, create a telecon object
     Telecon* telecon = new TeleconImplChartDir();
 
-    shared_ptr<TeleconControls> controls = telecon->addControls("MyControls");
-    controls->addButton("Button 1", printHello);
-    shared_ptr<TeleconToggle> run = controls->addToggle("Run");
-    // shared_ptr<TeleconToggle> toggle2 = controls->addToggle("Toggle 2");
-    shared_ptr<TeleconSlider> slider2 = controls->addSlider("Frequency", 0, 3, 1, 2);
-    shared_ptr<TeleconSlider> slider3 = controls->addSlider("Amplitude", 0, 3, 2, 2);
-    shared_ptr<TeleconInput> input1 = controls->addInput("Input 1", 300);
-    input2 = controls->addInput("Input 2", 10);
-    shared_ptr<TeleconInput> input3 = controls->addInput("amplitude", 1);
 
-    shared_ptr<TeleconWindow> sineDemoWindow = telecon->addWindow("sineDemoWindow");
 
-    shared_ptr<TeleconRealtimeChart> sinechart = sineDemoWindow->addRealtimeChart("Chart1", 60.0, dataRateMillis, "time (s)", "lbs");
+    ///// CONTROLS SETUP BEGIN
+    // A CONTROL window can have BUTTONS, TOGGLES, SLIDERS, and INPUTS
+
+    // Create a controls window
+    shared_ptr<TeleconControls> controls = telecon->addControls("MyControls");     // For windows, add it directly to the created Telecon object
+
+    // Different control elements
+    // For control elements, use add[element] on your created TeleconControls object
+    controls->addButton("Button 1", printHello);     // You can add a button that is linked to a function (in this case we have printHello()
+    shared_ptr<TeleconToggle> run = controls->addToggle("Run");     // You can also add a toggle button
+    shared_ptr<TeleconSlider> slider2 = controls->addSlider("Frequency", 0, 3, 1, 2);     // Here is a slider with min value 0 and max value 3, starting at 1 with precision 2
+    shared_ptr<TeleconSlider> slider3 = controls->addSlider("Amplitude", 0, 3, 2, 2);     // Here is another slider, you can add multiple elements of the same type
+    shared_ptr<TeleconInput> input1 = controls->addInput("Input 1", 300);     // Here is an input field with a default value of 300
+    shared_ptr<TeleconInput> input2 = controls->addInput("Input 2", 10);     // Another input field with default value 10
+    shared_ptr<TeleconInput> input3 = controls->addInput("amplitude", 1);     // A third input field
+
+    // Create a normal window to be controlled by the previously created controls window
+    shared_ptr<TeleconWindow> sineDemoWindow = telecon->addWindow("sineDemoWindow");     // Since this is a window, add directly to Telecon object
+
+    // Create a plot that displays updates from the controls window
+    // First create a chart, since all plots need to be displayed in a chart
+    shared_ptr<TeleconRealtimeChart> sinechart = sineDemoWindow->addRealtimeChart("Chart1", 60.0, dataRateMillis, "time (s)", "lbs");     //add charts to window objects
+    // Then we can add a plot to an existing chart
     shared_ptr<TeleconLinePlot> sinePlot = sinechart->addLinePlot("Sine Function", LineStyle(COLOR_BLACK));
     double sinvalue = 0;
 
-    // telecon->teleconStart();
-    // wxDateTime start = wxDateTime::UNow();
-    // double startTimestamp = Chart::chartTime2(start.GetTicks()) + start.GetMillisecond() / 1000.0;
-    // while (true) {
-    //     wxDateTime now = wxDateTime::UNow(); // Needs to use UNow instead of Now for millisecond precision
-    //     double nowTimestampAbsolute = Chart::chartTime2(now.GetTicks()) + now.GetMillisecond() / 1000.0;
-    //     double nowTimestampRelative = nowTimestampAbsolute - startTimestamp;
+    ///// CONTROLS SETUP END
 
-    //     sinePlot->pushData(nowTimestampRelative, sin(nowTimestampRelative * slider2->getCurrentValue()));
 
-    //     if (telecon->hasStopped()) {
-    //         break;
-    //     }
-    //     this_thread::sleep_for(chrono::milliseconds(dataRateMillis));
-    // }
-    // telecon->teleconStart();
-    // while(true){
-    //     this_thread::sleep_for(chrono::milliseconds(200));
-    //     // cout << "Toggle 1 state: " << toggle1->state << "    Toggle 2 state: " << toggle2->state << endl;
-    //     cout << "Input 1 state: " << input1->getVal() << endl;
-    //     cout << "Input 2 state: " << input2->getVal() << endl;
-    //     // cout << "Input 3 state: " << input1->getVal() << endl;
-    // }
 
+    ///// TELEMETRY SETUP BEGIN
+    // Hierarchy of elements: A WINDOW has CHARTS which contain PLOTS
+    // A WINDOW may have different types of CHARTS, but it is best practice that CHARTS only contain the same type PLOTS
+
+    // Create a window [1]
     shared_ptr<TeleconWindow> window = telecon->addWindow("MyWindow");
 
+    // Create a Realtime Chart for Line Plots
     shared_ptr<TeleconRealtimeChart> chart1 = window->addRealtimeChart("Chart1", 60.0, dataRateMillis, "time (s)", "lbs");
+    // Add plots to the created chart
     shared_ptr<TeleconLinePlot> plot1 = chart1->addLinePlot("Expected Tension", LineStyle(COLOR_BLACK));
     shared_ptr<TeleconLinePlot> plot2 = chart1->addLinePlot("Force", LineStyle(COLOR_GREEN, 5, LT_SOLID));
 
-    SymbolStyle noSymbol(SYMBOL_NO_SYMBOL);
+    // Create another Realtime Chart for Line Plots
     shared_ptr<TeleconRealtimeChart> chart2 = window->addRealtimeChart("Second Chart", 60.0, dataRateMillis, "time (s)", "inches");
+    // Add plots to this chart
     shared_ptr<TeleconLinePlot> plot3 = chart2->addLinePlot("Extension (expected)", LineStyle(COLOR_DEFAULT), SymbolStyle(SYMBOL_CIRCLE, COLOR_DEFAULT, true, 5));
     shared_ptr<TeleconLinePlot> plot4 = chart2->addLinePlot("Extension (actual)", LineStyle(COLOR_GREEN, 1, LT_DASHED));
-    //test for legends
-    SymbolStyle circleStyle;
+    SymbolStyle circleStyle;  // create a symbol object for plot point display custimization
     shared_ptr<TeleconLinePlot> plot5 = chart2->addLinePlot("Extension (expected) dup1", LineStyle(COLOR_RED), circleStyle);
     shared_ptr<TeleconLinePlot> plot6 = chart2->addLinePlot("Extension (expected) dup2", LineStyle(COLOR_GREEN), circleStyle);
     shared_ptr<TeleconLinePlot> plot7 = chart2->addLinePlot("Extension (actual) dup1", LineStyle(COLOR_BLUE, 1, LT_DASHED));
     shared_ptr<TeleconLinePlot> plot8 = chart2->addLinePlot("Extension (actual) dup2", LineStyle(COLOR_DEFAULT, 1, LT_DASHED));
 
+    // Create another window [2]
     shared_ptr<TeleconWindow> window2 = telecon->addWindow("Second Window");
 
+    // Similarly create a chart and add plots
     shared_ptr<TeleconRealtimeChart> chart3 = window2->addRealtimeChart("Diverging Chart", 6.0, dataRateMillis, "time (s)", "ft", CSM_DIVERGING);
     shared_ptr<TeleconLinePlot> plot9 = chart3->addLinePlot("Plot 1");
     shared_ptr<TeleconLinePlot> plot10 = chart3->addLinePlot("Plot 2");
     shared_ptr<TeleconLinePlot> plot11 = chart3->addLinePlot("Plot 3");
     shared_ptr<TeleconLinePlot> plot12 = chart3->addLinePlot("Plot 4");
 
+    // Another Realtime Chart, but this time with Scatter Plots
     shared_ptr<TeleconRealtimeChart> chart4 = window2->addRealtimeChart("Scatter Chart", 6.0, dataRateMillis, "time (s)", "mph", CSM_DIVERGING);
     shared_ptr<TeleconScatterPlot> plot13 = chart4->addScatterPlot("Speed", SymbolStyle(SYMBOL_CROSS, COLOR_DEFAULT, false));
     shared_ptr<TeleconScatterPlot> plot14 = chart4->addScatterPlot("New one", SymbolStyle(SYMBOL_CIRCLE, COLOR_DEFAULT, false));
     shared_ptr<TeleconScatterPlot> plot15 = chart4->addScatterPlot("Velocity", SymbolStyle(SYMBOL_DIAMOND, COLOR_BLUE, true, 3));
 
+    // Create another window [3]
     shared_ptr<TeleconWindow> window3 = telecon->addWindow("Third Window");
 
+    // Create a Realtime Chart, but this time for Raster Plots.
     shared_ptr<TeleconRealtimeChart> chart5 = window3->addRealtimeChart("Raster Chart", 60.0, dataRateMillis, "time (s)", "Neuron", CSM_BLACK);
     
+    // Set up Raster Plots. 
+    // They are also added to the chart like the other plots, but due to the nature of the plot the setup may be different
+    // How you store the shared pointers is up to you
     vector<shared_ptr<TeleconRasterPlot>> rasterPlots;
     for (int i = 0; i < 100; ++i) {
         rasterPlots.push_back(chart5->addRasterPlot("", i, SymbolStyle(SYMBOL_SQUARE, COLOR_RED, false, 3)));
@@ -128,6 +149,7 @@ int main(int argc, char* argv[])
         rasterPlots.push_back(chart5->addRasterPlot("", i));
     }
 
+    // Now create a Data Chart to add Temporal Phse Portrait Plots
     shared_ptr<TeleconDataChart> chart6 = window3->addDataChart("Temporal Phase Portrait", 6.0, dataRateMillis, "amount spent ($)", "grade (%)", CSM_DIVERGING);
     vector<shared_ptr<TeleconPhasePortraitPlot>> phasePortraits;
     phasePortraits.push_back(chart6->addPhasePortraitPlot("Student A"));
@@ -135,27 +157,31 @@ int main(int argc, char* argv[])
     phasePortraits.push_back(chart6->addPhasePortraitPlot("Student C"));
     phasePortraits.push_back(chart6->addPhasePortraitPlot("Student D"));
 
-    telecon->teleconStart();
-    telecon->teleconStart();
-
-    // while(true){
-    //     this_thread::sleep_for(chrono::milliseconds(200));
-    //     // cout << "Toggle 1 state: " << toggle1->state << "    Toggle 2 state: " << toggle2->state << endl;
-    //     cout << "Slider 1 state: " << slider1->current_state << endl;
-    // }
-
-    telecon->addWindow("Third window");
+    // Create another window [4]
+    telecon->addWindow("Fourth window");
     window2->addRealtimeChart();
     chart4->addLinePlot("line");
     chart4->addScatterPlot("scatter");
 
+    // Storing previously created plots for easier access, but this portion is not necessary
     vector<shared_ptr<TeleconLinePlot>> absoluteTimePlots({ plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8, plot9, plot10, plot11, plot12});
     vector<shared_ptr<TeleconScatterPlot>> relativeTimePlots({ plot13, plot14, plot15 });
 
-    // Controller code starts here
+    ///// TELEMETRY SETUP END
+
+
+
+    // Make sure to start Telecon!
+    telecon->teleconStart();
+
+
+
+    ///// CONTROLLER CODE BEGIN
+    // This portion is to generate data to push into the plots
     wxDateTime start = wxDateTime::UNow();
     double startTimestamp = Chart::chartTime2(start.GetTicks()) + start.GetMillisecond() / 1000.0;
 
+    // Loop until Telecon has stopped
     while (true) {
         wxDateTime now = wxDateTime::UNow(); // Needs to use UNow instead of Now for millisecond precision
 
@@ -163,6 +189,7 @@ int main(int argc, char* argv[])
         // Since that loses millisecond precision, add it back in with GetMillisecond()
         double nowTimestampAbsolute = Chart::chartTime2(now.GetTicks()) + now.GetMillisecond() / 1000.0;
         double nowTimestampRelative = nowTimestampAbsolute - startTimestamp;
+
         for (auto absoluteTimePlot : absoluteTimePlots) {
             absoluteTimePlot->pushData(nowTimestampAbsolute, CreateDataPoints());
         }
@@ -185,12 +212,17 @@ int main(int argc, char* argv[])
         if (telecon->hasStopped()) {
             break;
         }
+
         this_thread::sleep_for(chrono::milliseconds(dataRateMillis));
+
     }
 
-    // Controller code ends here
+    ///// CONTROLLER CODE END
 
+
+    // Make sure to stop Telecon as well
     telecon->teleconStop();
+
 
     delete telecon;
 
